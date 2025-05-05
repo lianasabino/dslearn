91a88e2aa827216a60c5363838e1d190e019f0d1
lianasabino <lianasabino@hotmail.com>
2025-05-05 11:52:16 -0300

    Resource

--- src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearn.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
@@ -10,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,6 +30,9 @@ public class Offer implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
 	
 	public Offer() {}
 
@@ -77,6 +83,11 @@ public class Offer implements Serializable {
 
 	public void setCourse(Course course) {
 		this.course = course;
+	}
+	
+
+	public List<Resource> getResources() {
+		return resources;
 	}
 
 	@Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Url, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-07-13T20:50:07.00Z', TIMESTAMP WITH TIME ZONE '2026-07-13T20:50:07.00Z', 1 );
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-09-13T20:50:07.00Z', TIMESTAMP WITH TIME ZONE '2026-09-13T20:50:07.00Z', 1 );
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 1, 1 );
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 2, 1 );
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 0, 1 );
